306a044eaeff8d57ae402bc4b55a82cab650907f
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-03-01 15:30:27 +0800

    Fixed crashing while running on cmd

--- .completed/calculator/simplecalculator.py
+++ .completed/calculator/simplecalculator.py
@@ -25,29 +25,33 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-while True:
-    # Take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+def runtime():
 
-    # Check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+    while True:
+        # Take input from the user
+        choice = input("Enter choice(1/2/3/4): ")
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+        # Check if choice is one of the four options
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        # break
-        # Add break if you only want to execute code once, left it off
-        # because it auto terminates when initiated in the Terminal.
-    else:
-        print("Invalid Input")
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
+
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
+            # break
+            # Add break if you only want to execute code once, left it off
+            # because it auto terminates when initiated in the Terminal.
+        else:
+            print("Invalid Input")
+
+runtime()
 
