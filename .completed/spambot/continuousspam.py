eccc8974a5462c2cd99fee5f95d0cdea4a9c38d4
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-03-03 20:39:28 +0800

    Updated file directory

--- .completed/spambot/continuousspam.py
+++ .completed/spambot/continuousspam.py
@@ -2,7 +2,7 @@
 import pyautogui, time
 
 time.sleep(1) # Lowered because of lower text filesize.
-f = open(r"C:\Users\Flores\Joey-vscode-workspaces\General-Python-Repository\.projects\.completed\spambot\simplespam.txt", "r")
+f = open(r"C:\Users\Flores\Joey-vscode-workspaces\Python-Projects\.completed\spambot\simplespam.txt", "r")
 
 for word in f:
     pyautogui.typewrite(word)
