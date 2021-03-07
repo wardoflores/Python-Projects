29013ee5294a54a73d159bbe1a62da6106c62f31
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-03-08 04:28:45 +0800

    Create pyproj.bat

---
+++ pyproj.bat
@@ -0,0 +1,30 @@
+echo off
+echo =================================================================
+echo .
+echo Hello (~*v*)~
+echo Run using aliases for convenience.
+echo .
+echo =================================================================
+echo .
+echo Current Python scripts are:
+echo .
+echo organize "path"
+echo keylogger
+echo calculator
+echo simplespam
+echo navyspam
+echo birthday
+echo .
+echo =================================================================
+echo .
+echo Current batch scripts are:
+echo .
+echo root
+echo .
+echo =================================================================
+doskey root=cd C:\Windows\system32
+doskey calculator=python "calculator project directory"
+doskey simplespam=python "simplespam project directory"
+doskey navyspam=python "navyspam project directory"
+doskey keylogger=python "keylogger project directory"
+doskey birthday=python "birthday project directory"
