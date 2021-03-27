fe81fb682025044983f170375b1fbdd8b8e201d0
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-03-28 01:03:57 +0800

    Improved code

--- completed/spambot/continuousspam.py
+++ completed/spambot/continuousspam.py
@@ -5,8 +5,12 @@
 import pyautogui, time
 
 time.sleep(1) # Lowered because of lower text filesize.
-f = open(r"C:\Users\Flores\Joey-Repositories\Python-Projects\completed\spambot\simplespam.txt", "r")
+while True:
+    f = open(r"C:\Users\Flores\Joey-Repositories\Python-Projects\completed\spambot\simplespam.txt", "r")
 
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
+    for word in f:
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
+
+    if KeyboardInterrupt:
+        False
