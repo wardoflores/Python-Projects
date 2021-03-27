3bfac8beaa75bd0d24ab4dc90ce6c526e5f15079
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-03-28 01:03:47 +0800

    Improved code

--- completed/meditation/meditate.py
+++ completed/meditation/meditate.py
@@ -4,51 +4,66 @@ import time
 Guides you through the process of meditation.
 '''
 
+print("\n")
 print("Good day to you my friend.")
+print("\n")
 time.sleep(1)
 
 
 print("We'll go through the meditative process now, are you ready?")
 questionone = input("Type Yes: ")
 if questionone == "Yes":
+    print("\n")
     print("Great!")
 else:
     print("Well I will still go on as needed, knowing that you came.")
     
 time.sleep(2)
+print("\n")
 
 print("make yourself comfortable in a space without distractions.")      
 time.sleep(5)
+print("\n")
 
 
-print("Let's reflect on these concepts.")
+print("Let's reflect on these concepts:")
+print("\n")
 time.sleep(2)
 
 def process():
     print("Who are you?")
+    print("\n")
     time.sleep(5)
 
 
     print("What mindset do you primarily have right now?")
+    print("\n")
     time.sleep(5)
 
 
     print("Recall your principles.")
+    print("\n")
     time.sleep(5)
 
 
     print("Reflect on your Spirituality.")
+    print("\n")
     time.sleep(5)
 
 
     print("What are your Responsibilities?")
+    print("\n")
     time.sleep(5)
+
 process()
 
-restart = input("Do you want to be reminded again? Type Yes to be reminded: ")
+restart = input("Do you want to be reminded once again? Type Yes to be reminded: ")
 if restart == "Yes":
+    print("\n")
     restart ==  process()
 else:
+    print('\n')
     print("Excellent, have a nice day!")
     exit()
+
 
