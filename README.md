af0238a06720fc742519c9b4a31b8407efcf7ce0
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-05-04 11:24:30 +0800

    Added auto clicker

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Add the project module's directories to the pyproj.bat file.
 
 * meditation - made by myself
 * speedprinter - made by myself
+* Clicker - https://www.pythonpool.com/python-auto-clicker/
 * Calculator - https://www.programiz.com/python-programming/examples/calculator
 * File Organizer - gagangulyani: https://github.com/gagangulyani/File-Organizer
 * Spam Bot - youtube.com/watch?v=jBxRGcDmfWA / https://www.youtube.com/watch?v=-zd1UI2JTuk
---
+++ completed/autoclicker/clicker.py
@@ -0,0 +1,55 @@
+import time
+import threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+ 
+ 
+delay = 0.001
+button = Button.left
+start_stop_key = KeyCode(char='s')
+exit_key = KeyCode(char='e')
+ 
+ 
+class ClickMouse(threading.Thread):
+    def __init__(self, delay, button):
+        super(ClickMouse, self).__init__()
+        self.delay = delay
+        self.button = button
+        self.running = False
+        self.program_run = True
+ 
+    def start_clicking(self):
+        self.running = True
+
+    def stop_clicking(self):
+        self.running = False
+ 
+    def exit(self):
+        self.stop_clicking()
+        self.program_run = False
+ 
+    def run(self):
+        while self.program_run:
+            while self.running:
+                mouse.click(self.button)
+                time.sleep(self.delay)
+            time.sleep(0.1)
+ 
+ 
+mouse = Controller()
+thread = ClickMouse(delay, button)
+thread.start()
+ 
+ 
+def on_press(key):
+    if key == start_stop_key:
+        if thread.running:
+            thread.stop_clicking()
+        else:
+            thread.start_clicking()
+    elif key == exit_key:
+        thread.exit()
+        listener.stop()
+ 
+with Listener(on_press=on_press) as listener:
+    listener.join()
