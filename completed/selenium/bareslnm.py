be8263410d0ccd496eb221ec15cf62d791eb8c26
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-05-11 00:01:32 +0800

    Optimized code

--- completed/selenium/bareslnm.py
+++ completed/selenium/bareslnm.py
@@ -4,13 +4,16 @@ import time
 driver_path = r"C:\webdriver\chromedriver.exe"
 brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
 
-option = webdriver.ChromeOptions()
-option.binary_location = brave_path
-# option.add_argument("--incognito") OPTIONAL
-# option.add_argument("--headless") OPTIONAL
+opt = Options()
+opt.binary_location = brave_path
+# opt.add_argument("--incognito") OPTIONAL
+# opt.add_argument("--headless") OPTIONAL
+opt.add_argument('--start-maximized')
+opt.add_experimental_option('excludeSwitches', ['enable-logging'])
+opt.add_argument('--disable-blink-features=AutomationControlled')
 
 # Create new Instance of Chrome
-browser = webdriver.Chrome(executable_path=driver_path, chrome_options=option)
+browser = webdriver.Chrome(executable_path=driver_path, options=opt)
 
 browser.get("https://www.google.com")
 
