8ec8b56c2cbf4abf69c5e9a6d81156b7fd3952ed
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-07-17 10:58:15 +0800

    Cleaned code blueprint

--- inprogress/taskscheduler/taskscheduler.py
+++ inprogress/taskscheduler/taskscheduler.py
@@ -1,6 +1,6 @@
-import tkinter
-
-
 # code a simple ui that schedules tasks;
+import tkinter
 # code a database to store those tasks;
+import sqlite3
 # organize the database to accomodate the various data types needed in the application
+import sqlalchemy
