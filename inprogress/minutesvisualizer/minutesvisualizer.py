49f4a73f07941d86941c551acc59892aba98cd80
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-07-17 10:14:40 +0800

    Cleaned up code blueprint

--- inprogress/minutesvisualizer/minutesvisualizer.py
+++ inprogress/minutesvisualizer/minutesvisualizer.py
@@ -1,12 +1,13 @@
-import tkinter
-
 # code an application that visualizes the key events made at a meeting;
 
- 
-# code for database for minutes data storage;
+# code backend for visualization
+import turtle
 
+# code for database for minutes data storage;
+import sqlite3
 
 # code a ui for ease of access;
-
-
 # code a gui for visualization of minutes of a meeting;
+import tkinter
+
+
