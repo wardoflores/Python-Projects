0f483e510f97726474313ba67b0bd42620b38799
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-08-19 21:31:08 +0800

    Update meetauto.bat

--- completed/selenium/meetauto.bat
+++ completed/selenium/meetauto.bat
@@ -5,6 +5,6 @@ echo Started: %date% %time%
 python "C:\Users\Flores\Joey-Repositories\Python-Projects\completed\selenium\meetopener.py"
 echo Ending script.
 echo Completed: %date% %time%
-timeout /t 7200 /nobreak > nul
-@REM wait for 2 hours
+timeout /t 3000 /nobreak > nul
+@REM wait for 50 minutes
 goto loop
