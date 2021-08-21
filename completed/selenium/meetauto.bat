71c03fdc0388a1165517808a04ae9e35f182443b
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-08-21 18:17:31 +0800

    Added weekend detection

--- completed/selenium/meetauto.bat
+++ completed/selenium/meetauto.bat
@@ -2,8 +2,13 @@ echo off
 @REM TODO write log in txt file
 :loop
 cls
-echo Starting script...
 echo Started: %date% %time%
+echo Exiting script if today is a weekend...
+echo.
+for /f %%i in ('powershell ^(get-date^).DayOfWeek') do set dow=%%i
+if %dow% == Saturday goto exit 
+if %dow% == Sunday goto exit
+echo Starting script...
 python "C:\Users\Flores\Joey-Repositories\Python-Projects\completed\selenium\meetopener.py"
 echo.
 echo Ending script...
@@ -11,3 +16,5 @@ echo Completed: %date% %time%
 echo waiting for 40 minutes...
 timeout /t 2400 /nobreak > nul
 goto loop
+:exit
+exit
