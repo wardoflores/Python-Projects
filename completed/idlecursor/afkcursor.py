2c0eb8813e56abe1daabfa57ab6817d9ad656121
Joey Flores <61069122+Coalemus@users.noreply.github.com>
2022-02-01 01:41:26 +0800

    renamed file

--- completed/idlecursor/afkcursor.py
+++ completed/idlecursor/afkcursor.py
@@ -1,11 +1,10 @@
+#!/usr/bin/zsh
+
 import pyautogui
 import time
-
-# Buggy on Linux Sway WM.
 
 while True:
         
     pyautogui.moveTo(720, 100, 2)   # moves mouse to X of 100, Y of 200 over 2 seconds
     time.sleep(3) # Breathing room for errors
     pyautogui.moveTo(720, 200, 2)   # moves mouse to X of 100, Y of 200 over 2 seconds
-
