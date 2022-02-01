35419aa2aa7bed2b31c7b9b7f4800871884abf51
Joey Flores <61069122+Coalemus@users.noreply.github.com>
2022-02-02 06:45:47 +0800

    Added Program from TKDocs to refer to in TK GUI projects.

---
+++ completed/calculator/feettometergui.py
@@ -0,0 +1,50 @@
+#!/bin/zsh
+# Program from TKDocs to refer to in TK GUI projects.
+# Source: https://tkdocs.com/tutorial/firstexample.html
+# Eduardo Jose Flores III: https://github.com/Coalemus
+# Copyright (c) Eduardo Jose Flores III
+
+from tkinter import *
+from tkinter import ttk
+
+# Set up main application window
+root = Tk() # initialize window
+root.title("Feet to Meters")
+
+# Calculation algorithm
+def calculate(*args):
+    try:
+        value = float(feet.get()) # Get string var from the input of the entry in the mainframe
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0) # Calculation algo
+    except ValueError:
+        pass
+
+# Create content frame which will hold the contents of our user interface.
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+# First widget
+feet = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+# Remaining widgets
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+# UI Polishing
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+feet_entry.focus()
+root.bind("<Return>", calculate)
+
+# Start the event loop
+root.mainloop()
