821c360874b35acdbcf7b4c7d384245a6c870012
Joey Flores <61069122+Coalemus@users.noreply.github.com>
2022-02-03 14:21:08 +0800

    Update taskscheduler.py

--- inprogress/taskscheduler/taskscheduler.py
+++ inprogress/taskscheduler/taskscheduler.py
@@ -1,6 +1,16 @@
 # code a simple ui that schedules tasks;
-import tkinter
+from tkinter import *
+from tkinter import ttk
 # code a database to store those tasks;
 import sqlite3
 # organize the database to accomodate the various data types needed in the application
 import sqlalchemy
+
+root = Tk()
+root.title("Task Scheduler")
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+mainframe = ttk.Frame(root, padding="100 100 100 100")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
