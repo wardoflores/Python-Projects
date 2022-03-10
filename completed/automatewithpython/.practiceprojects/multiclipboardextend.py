3587ac4580735083e48d770fe90157d810fbb9c9
Joey Flores <61069122+Coalemus@users.noreply.github.com>
2022-03-10 20:41:42 +0800

    organized automate with python folders

--- completed/automatewithpython/.practiceprojects/multiclipboardextend.py
+++ completed/automatewithpython/.practiceprojects/multiclipboardextend.py
@@ -5,3 +5,4 @@ Extend the multiclipboard program in this chapter so that it has a delete <keywo
 command line argument that will delete a keyword from the shelf. Then add a delete
 command line argument that will delete all keywords.
 '''
+
