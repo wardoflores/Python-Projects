513454be95e7ae28b0afd4db8bd4c6fb10e754db
Joey Flores <61069122+Coalemus@users.noreply.github.com>
2022-04-10 12:56:32 +0800

    Update sumofallnums.py

--- interview/sumofallnums.py
+++ interview/sumofallnums.py
@@ -1,3 +1,17 @@
 # Outputs the sum of the range of all numbers from 1 to n.
 
 # Source: https://pynative.com/python-program-to-calculate-sum-and-average-of-numbers/
+
+input_string = input('Enter numbers separated by space ')
+print("\n")
+# Take input numbers into list
+numbers = input_string.split()
+
+# convert each item to int type
+for i in range(len(numbers)):
+    # convert each item to int type
+    numbers[i] = int(numbers[i])
+
+# Calculating the sum and average
+print("Sum = ", sum(numbers))
+print("Average = ", sum(numbers) / len(numbers))
