d2559c253c7ad1a37acc9061c95fc8e171cb9b81
wardoflores <eduardojoeyflores@gmail.com>
2024-06-24 02:20:32 +0800

    fully functional

--- random/numberguesser.py
+++ random/numberguesser.py
@@ -2,20 +2,27 @@
 # Automate the Boring Stuff with Python
 import random
 
-secretnumber = random.randint(1,20)
 print("I am thinking of a number between 1 and 20")
 
-for guessestaken in range(1,7):
-    guess = 0
-    print("Take a guess.")
-    guess = int(input())
+def main():
+    count=0
+    for guessestaken in range(1,7):
+        count+=1
+        secretnumber = random.randint(1,20)
+        print("Take a guess.")
+        guess = int(input())
 
-    if guess < secretnumber:
-        print("Your guess is too low.")
-    elif guess > secretnumber:
-        print("Your guess is too high.")
-    elif guess == secretnumber:
-        print("Good job! You guessed my number in " + str(guessestaken) + " guesses!")
-    else:
-        print("Nope. The number I was thinking of was " + str(secretnumber))
-
+        if guess < secretnumber:
+            print("Your guess is too low.")
+            if count == 6:
+                print("Nope. The number I was thinking of was " + str(secretnumber))
+        elif guess > secretnumber:
+            print("Your guess is too high.")
+            if count == 6:
+                print("Nope. The number I was thinking of was " + str(secretnumber))
+        elif guess == secretnumber:
+            print("Good job! You guessed my number in " + str(guessestaken) + " guesses!")
+            break
+        else:
+            print("Nope. The number I was thinking of was " + str(secretnumber))
+main()
